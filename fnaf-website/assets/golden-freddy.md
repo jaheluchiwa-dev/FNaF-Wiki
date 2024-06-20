46505e8be23d991fd27f15f333a236b2c3088db7
Jajou64 <jaheluchiwa64@gmail.com>
2024-06-20 22:55:14 +0200

    UPDATE JS

--- fnaf-website/assets/golden-freddy.md
+++ fnaf-website/assets/golden-freddy.md
@@ -10,87 +10,9 @@
       goldenFreddySound.src = "./assets/sounds/golden-freddy-it's-me-sound.mp3";
       goldenFreddySound.play();
 
-      // Arrêter le son de Freddy
-      freddySound.pause();
-      kitchenNoice.pause();
-      foxyDumDum.pause();
-      bonnieMoans.pause();
-
-      function removeItsMe() {
-        const itsMeElements = document.querySelectorAll(".its-me");
-        itsMeElements.forEach(function (element) {
-          element.remove();
-        });
-      }
 
       // SPAM ItsMe
-      function createIM() {
-        removeItsMe();
-
-        for (let i = 0; i < 50; i++) {
-          const animatronicProfile = document.querySelector(
-            ".animatronic-profile"
-          );
-
-          if (animatronicProfile) {
-            const spamItsMe = document.createElement("p");
-
-            let randomItsMe = 0;
-            randomItsMe = Math.random() * 100;
-
-            spamItsMe.style.top = Math.random() * 90 + "%";
-            spamItsMe.style.left = Math.random() * 90 + "%";
-            spamItsMe.style.fontSize = Math.random() * 3 + "em";
-            spamItsMe.style.opacity = Math.random() * 0.5;
-
-            if (Math.floor(Math.random() * 2) === 0) {
-              spamItsMe.style.display = "none";
-            }
-
-            spamItsMe.textContent = "It's Me".toUpperCase();
-
-            spamItsMe.classList.add("its-me");
-            bodyBackground.appendChild(spamItsMe);
-          }
-        }
-      }
-
-      createIM();
-      // Un interval tout les 100 miliseconds 50 "ItsMe" apparaissent
-      itsMeUpdate = setInterval(createIM, 100);
-
-      jumpcareImg.classList.remove("jumpscare");
-      animatronicImg.classList.add("golden-freddy");
-
-      // Ajout - Remove des class
-      goldenFreddyBtn.classList.add("active");
-
-      bodyBackground.classList.add("gf-background");
-
-      bodyBackground.classList.remove("foxy-background");
-      bodyBackground.classList.remove("freddy-background");
-      bodyBackground.classList.remove("bonnie-background");
-      bodyBackground.classList.remove("chica-background");
-      bodyBackground.classList.remove("background");
-
-      chicaBtn.classList.remove("active");
-      bonnieBtn.classList.remove("active");
-      freddyBtn.classList.remove("active");
-      foxyBtn.classList.remove("active");
-
-
-
-
-
-          function removeItsMe() {
-      const itsMeElements = document.querySelectorAll(".its-me");
-      itsMeElements.forEach(function (element) {
-        element.remove();
-      });
-    }
 
 
     //Spamm It's Me
-const spamItsMe = document.createElement("p");
 
-let itsMeUpdate;
--- fnaf-website/assets/js/animatronics.js
+++ fnaf-website/assets/js/animatronics.js
@@ -16,10 +16,11 @@ const description = document.createElement("p");
 const animatronicName = document.createElement("h1");
 const fnaf = document.createElement("p");
 const nights = document.createElement("p");
+const spamItsMe = document.createElement("p");
 
 // Les sons des animatroniques avec leurs jumpscares
 const animatronicsAudio = document.createElement("audio");
-const animatronicsJumpscareAudio = document.createElement('audio')
+const animatronicsJumpscareAudio = document.createElement("audio");
 
 //Fetch du data.json
 fetch("./assets/data/data.json")
@@ -29,12 +30,56 @@ fetch("./assets/data/data.json")
   .then(function (data) {
     console.log(data);
 
+    // Fonction pour enlever les Its-me
+    function removeItsMe() {
+      const itsMeElements = document.querySelectorAll(".its-me");
+      itsMeElements.forEach(function (element) {
+        element.remove();
+      });
+    }
+
+    function createIM() {
+      removeItsMe();
+
+      for (let i = 0; i < 50; i++) {
+        const animatronicProfile = document.querySelector(
+          ".animatronic-profile"
+        );
+
+        if (animatronicProfile) {
+          const spamItsMe = document.createElement("p");
+
+          let randomItsMe = 0;
+          randomItsMe = Math.random() * 100;
+
+          spamItsMe.style.top = Math.random() * 90 + "%";
+          spamItsMe.style.left = Math.random() * 90 + "%";
+          spamItsMe.style.fontSize = Math.random() * 3 + "em";
+          spamItsMe.style.opacity = Math.random() * 0.5;
+
+          if (Math.floor(Math.random() * 2) === 0) {
+            spamItsMe.style.display = "none";
+          }
+
+          spamItsMe.textContent = "It's Me".toUpperCase();
+
+          spamItsMe.classList.add("its-me");
+          bodyBackground.appendChild(spamItsMe);
+        }
+      }
+    }
+
+    createIM();
+    // Un interval tout les 100 miliseconds 50 "ItsMe" apparaissent
+    let itsMeUpdate;
+    itsMeUpdate = setInterval(createIM, 100);
+
     // Quand tu cliques sur l'image de l'animatronique ça lance un jumpscare
-    animatronicImg.addEventListener('click', function(){
-      animatronicImg.src = data[0].animatronic_jumpscare
-      animatronicsJumpscareAudio.src = data[0].jumpscare_audio
-      animatronicsJumpscareAudio.play()
-    })
+    animatronicImg.addEventListener("click", function () {
+      animatronicImg.src = data[0].animatronic_jumpscare;
+      animatronicsJumpscareAudio.src = data[0].jumpscare_audio;
+      animatronicsJumpscareAudio.play();
+    });
 
     // Afficher Freddy
     description.textContent = data[0].description;
@@ -44,6 +89,7 @@ fetch("./assets/data/data.json")
     bodyBackground.style.backgroundImage = `url(${data[0].background})`;
     animatronicsAudio.src = data[0].sounds;
     animatronicsAudio.play();
+    removeItsMe()
 
     // Compteur pour les indices du data
     let compteur = 0;
@@ -55,12 +101,12 @@ fetch("./assets/data/data.json")
       animatronicsAudio.src = data[compteur].sounds;
       animatronicsAudio.play();
 
-      animatronicImg.addEventListener('click', function(){
-        animatronicImg.src = data[compteur].animatronic_jumpscare
-        animatronicsJumpscareAudio.src = data[compteur].jumpscare_audio
-        animatronicsJumpscareAudio.play()
-      })
-      animatronicsJumpscareAudio.pause()
+      animatronicImg.addEventListener("click", function () {
+        animatronicImg.src = data[compteur].animatronic_jumpscare;
+        animatronicsJumpscareAudio.src = data[compteur].jumpscare_audio;
+        animatronicsJumpscareAudio.play();
+      });
+      animatronicsJumpscareAudio.pause();
     }
 
     //Une fonction pour changer le texte dynamiquement
@@ -79,10 +125,10 @@ fetch("./assets/data/data.json")
       }
       camChangeAudio.src = "./assets/sounds/change-camera-sound.mp3";
       camChangeAudio.play();
-      console.log(camChangeAudio);
 
       updateImageAndSound();
       text();
+      
     });
 
     arrowRight.addEventListener("click", function () {
@@ -95,8 +141,6 @@ fetch("./assets/data/data.json")
       camChangeAudio.src = "./assets/sounds/change-camera-sound.mp3";
       camChangeAudio.play();
 
-      console.log(camChangeAudio);
-
       updateImageAndSound();
       text();
     });
@@ -106,7 +150,7 @@ fetch("./assets/data/data.json")
     fnaf.classList.add("fnaf");
     nights.classList.add("nights");
     animatronicName.classList.add("animatronic-name");
-    jumpcareImg.classList.add('jumpscare-image')
+    jumpcareImg.classList.add("jumpscare-image");
 
     // Ajouter au document
     animatronicProfile.appendChild(animatronicName);
